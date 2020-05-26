9c248382cf5417a383f3309015c9293dd3d1f40a
Abdulkadir Güler <59777388+abdulkadir69@users.noreply.github.com>
2020-05-26 18:17:35 +0300

    Update dışardan girilen sayının rakamları toplamını veren program.c

--- dışardan girilen sayının rakamları toplamını veren program.c
+++ dışardan girilen sayının rakamları toplamını veren program.c
@@ -6,13 +6,14 @@ int main()
 {
  int sayi,toplam=0,k,ilksayi;
 
- printf("sayi gir:");
+ printf("sayi gir :");
  scanf("%d",&sayi);
- ilksayi=sayi;
- while (sayi>0){
- 	k=sayi%10;
- 	sayi=sayi/10;
- 	toplam=toplam+k;
+ ilksayi = sayi;
+ while (sayi>0)
+ {
+ 	k = sayi % 10;
+ 	sayi = sayi / 10;
+ 	toplam = toplam + k;
  }
  printf("%d",toplam);
  getch();
